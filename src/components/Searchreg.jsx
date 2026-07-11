46997ec60ac27d5a6ce99a042ce9c143f84cdbc8
neehanazer <neehanaz226@gmail.com>
2026-07-11 11:24:22 +0530

    done

--- src/components/Searchreg.jsx
+++ src/components/Searchreg.jsx
@@ -3,7 +3,42 @@ import React from 'react'
 const Searchreg = () => {
   return (
     <div>
-      
+        
+      <div>
+      <div>
+      <div>
+        
+      <div className="container" style={{paddingTop:10,marginTop:50}}>
+        <h1 align="center" style={{marginBottom:50}}> Search Student</h1>
+        <div className="row justify-content-center ">
+            <div className="card shadow-lg p-4 rounded">
+            <div className="col-12-col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <div className="row g-3 ">
+                    <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">Registration Number</label>
+                        <input type="text" className="form-control" />
+
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+                        <label htmlFor="" className="form-label"></label>
+                        <button className="btn btn-primary">Search</button>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
     </div>
   )
 }
